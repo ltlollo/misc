6c1e3a8f53b3b98694242caa78023289d48f7ff2
ltlollo <ltlollo.it@gmail.com>
2014-09-30 20:07:46 +0200

    c++11

--- corner.cpp
+++ corner.cpp
@@ -12,11 +12,11 @@
 
 using namespace std;
 
-constexpr unsigned w_size{2}, n_features{1500};
+constexpr unsigned w_size{2}, n_features{2000};
 constexpr double ets{5000};
 
 struct feature {
-    struct pt { unsigned x, y;} p;
+    struct pt { size_t x, y;} p;
     double e;
 };
 
@@ -32,13 +32,13 @@ mgrad gradient(const png::image<png::gray_pixel>& img) {
     if (h < 3 || w < 3) {
         return grad;
     }
-    for (unsigned i{1}; i < h-1; ++i) {
-        for (unsigned j{1}; j < w-1; ++j) {
+    for (size_t i{1}; i < h-1; ++i) {
+        for (size_t j{1}; j < w-1; ++j) {
             grad[i][j].gxx = {-img[i][j-1] + img[i][j+1]}; // gx
         }
     }
-    for (unsigned i{1}; i < h-1; ++i) {
-        for (unsigned j{1}; j < w-1; ++j) {
+    for (size_t i{1}; i < h-1; ++i) {
+        for (size_t j{1}; j < w-1; ++j) {
             grad[i][j].gyy = {-img[i-1][j] + img[i+1][j]}; // gy
             grad[i][j].gxy = {grad[i][j].gyy*grad[i][j].gxx};
             grad[i][j].gyy *= grad[i][j].gyy;
@@ -69,24 +69,23 @@ int main(int argc, char *argv[]) {
     assert(argc > 1);
     png::image<png::gray_pixel> img(argv[1]);
     size_t h{img.get_height()}, w{img.get_width()};
-    assert(h > 2* w_size && w > 2*w_size);
+    assert(h > 2*w_size && w > 2*w_size);
     std::vector<feature> res;
     png::image<png::gray_pixel> out(w, h);
     auto grad = gradient(img);
-    for (unsigned i{1}; i < h-w_size-1; ++i) {
-        for (unsigned j{1}; j < w-w_size-1; ++j) {
+    for (size_t i{1}; i < h-w_size-1; ++i) {
+        for (size_t j{1}; j < w-w_size-1; ++j) {
             auto e = patch_interest(grad, i, j);
             if (e > ets) {
                 res.emplace_back(feature{{j+w_size/2,i+w_size/2}, e});
             }
-
         }
     }
-    std::sort(res.begin(), res.end(), [](const auto& f, const auto& s) {
+    std::sort(res.begin(), res.end(), [](const feature& f, const feature& s) {
         return f.e > s.e;
     });
     for_each(res.begin(), res.size() > n_features ?
-                 res.begin()+n_features : res.end(), [&](const auto& fe) {
+                 res.begin()+n_features : res.end(), [&](const feature& fe) {
         out[fe.p.y][fe.p.x] = 255;
     });
     out.write(string(argv[1])+"-out.png");
