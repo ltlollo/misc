fc68ad66f5fff1114c63c707f0ba14a89a8c3c49
ltlollo <ltlollo.it@gmail.com>
2014-10-01 09:34:33 +0200

    d_win configurable

--- corner.cpp
+++ corner.cpp
@@ -12,35 +12,38 @@
 
 using namespace std;
 
-constexpr unsigned w_size{2}, n_features{2000};
+constexpr unsigned w_size{2}, n_features{500}, d_win{1};
 constexpr double ets{5000};
+
+static_assert(d_win > 0, "invalid d_win");
+static_assert(w_size > d_win, "invalid w_size");
 
 struct feature {
     struct pt { size_t x, y;} p;
     double e;
 };
 
-struct g {
-    long gxx, gyy, gxy;
+struct pderiv {
+    int gxx, gyy, gxy;
 };
 
-using mgrad = std::vector<std::vector<g>>;
+using mgrad = std::vector<std::vector<pderiv>>;
 
 mgrad gradient(const png::image<png::gray_pixel>& img) {
     size_t h{img.get_height()}, w{img.get_width()};
-    mgrad grad(h, std::vector<g>(w, g()));
+    mgrad grad(h, std::vector<pderiv>(w, pderiv{0, 0, 0}));
     if (h < 3 || w < 3) {
         return grad;
     }
-    for (size_t i{1}; i < h-1; ++i) {
-        for (size_t j{1}; j < w-1; ++j) {
-            grad[i][j].gxx = {-img[i][j-1] + img[i][j+1]}; // gx
+    for (size_t i{d_win}; i < h-d_win; ++i) {
+        for (size_t j{d_win}; j < w-d_win; ++j) {
+            grad[i][j].gxx = img[i][j-d_win] - img[i][j+d_win]; // gx
         }
     }
-    for (size_t i{1}; i < h-1; ++i) {
-        for (size_t j{1}; j < w-1; ++j) {
-            grad[i][j].gyy = {-img[i-1][j] + img[i+1][j]}; // gy
-            grad[i][j].gxy = {grad[i][j].gyy*grad[i][j].gxx};
+    for (size_t i{d_win}; i < h-d_win; ++i) {
+        for (size_t j{d_win}; j < w-d_win; ++j) {
+            grad[i][j].gyy = img[i-d_win][j] - img[i+d_win][j]; // gy
+            grad[i][j].gxy = grad[i][j].gyy * grad[i][j].gxx;
             grad[i][j].gyy *= grad[i][j].gyy;
             grad[i][j].gxx *= grad[i][j].gxx;
         }
@@ -49,7 +52,7 @@ mgrad gradient(const png::image<png::gray_pixel>& img) {
 }
 
 inline double patch_interest(const mgrad& grad, unsigned y, unsigned x) {
-    g sum{0, 0, 0};
+    pderiv sum{0, 0, 0};
     for (unsigned k{0}; k < w_size; ++k) {
         for (unsigned z{0}; z < w_size; ++z) {
             sum.gxx += grad[y+k][x+z].gxx;
@@ -57,11 +60,11 @@ inline double patch_interest(const mgrad& grad, unsigned y, unsigned x) {
             sum.gxy += grad[y+k][x+z].gxy;
         }
     }
-    double tr = sum.gxx+sum.gyy;
+    double tr{sum.gxx+sum.gyy};
     if (!tr) {
-        return 0;
+        return 0.0;
     }
-    int det = sum.gxx*sum.gyy-sum.gxy*sum.gxy;
+    int det{sum.gxx*sum.gyy-sum.gxy*sum.gxy};
     return det/tr;
 }
 
@@ -69,13 +72,13 @@ int main(int argc, char *argv[]) {
     assert(argc > 1);
     png::image<png::gray_pixel> img(argv[1]);
     size_t h{img.get_height()}, w{img.get_width()};
-    assert(h > 2*w_size && w > 2*w_size);
+    assert(h > w_size+d_win && w > w_size+d_win);
     std::vector<feature> res;
     png::image<png::gray_pixel> out(w, h);
     auto grad = gradient(img);
-    for (size_t i{1}; i < h-w_size-1; ++i) {
-        for (size_t j{1}; j < w-w_size-1; ++j) {
-            auto e = patch_interest(grad, i, j);
+    for (size_t i{d_win}; i < h-w_size-d_win; ++i) {
+        for (size_t j{d_win}; j < w-w_size-d_win; ++j) {
+            double e = patch_interest(grad, i, j);
             if (e > ets) {
                 res.emplace_back(feature{{j+w_size/2,i+w_size/2}, e});
             }
@@ -84,11 +87,11 @@ int main(int argc, char *argv[]) {
     std::sort(res.begin(), res.end(), [](const feature& f, const feature& s) {
         return f.e > s.e;
     });
-    for_each(res.begin(), res.size() > n_features ?
-                 res.begin()+n_features : res.end(), [&](const feature& fe) {
+    std::for_each(res.begin(), res.size() > n_features
+                  ? res.begin()+n_features : res.end(), [&](const feature& fe) {
         out[fe.p.y][fe.p.x] = 255;
     });
-    out.write(string(argv[1])+"-out.png");
+    out.write(std::string(argv[1])+"-out.png");
     return 0;
 }
 
