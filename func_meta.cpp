127f9627bec45913f148697c9533068506ba4352
ltlollo <ltlollo.it@gmail.com>
2014-10-23 22:25:13 +0200

    fun: some more syntax change

--- func_meta.cpp
+++ func_meta.cpp
@@ -6,25 +6,28 @@ using namespace fun::cond;
 
 #define exp(...) __VA_ARGS__;
 #define if If<
-#define do >
+#define do >()
 #define else Else(),
 #define exec(...) int main(int argc, char *argv[]) { exp(__VA_ARGS__) exp(return 0) }
 #define set(x, y) using x = y;
 #define lambda(fun, ...) [](__VA_ARGS__) { exp(fun) }
+#define rlambda(fun, ...) lambda(return fun, __VA_ARGS__)
 #define def(name, fun, ...) exp(constexpr auto name = lambda(fun, __VA_ARGS__))
+#define rdef(name, fun, ...) def(name, return fun, __VA_ARGS__)
 
-def(add, return x + y, auto&& x, auto&& y)
-def(one, return 1)
 def(printnl, cout<<x<<'\n', auto&& x)
-def(other, return add(1, one()))
+rdef(add, x + y, auto&& x, auto&& y)
+rdef(one, 1)
+rdef(other, add(1, one()))
+rdef(str, to_string(x), auto&& x)
 
 set(Cond, False)
 exec(
   printnl(
-    if Cond do() (
-      lambda(return 1),
+    if Cond do (
+      rlambda(1),
         else
-      add, one(), other()
+      rlambda(add("hallo"s, str(other())))
     )
   )
 )
