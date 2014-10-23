5ac0bc6eaec1a6a2db60bbe1f6280d7b4c0a3b9d
ltlollo <ltlollo.it@gmail.com>
2014-10-23 20:30:55 +0200

    lambda

--- func_meta.cpp
+++ func_meta.cpp
@@ -4,27 +4,27 @@ using namespace std;
 using namespace fun;
 using namespace fun::cond;
 
-constexpr auto add = [](auto&& x, auto&& y) {
-    return x + y;
-};
-
-auto printnl = [](auto&& x) {
-    cout << x << '\n';
-};
-
+#define exp(...) __VA_ARGS__;
 #define if If<
 #define do >
 #define else Else(),
-#define exec(...) int main(int argc, char *argv[]) { __VA_ARGS__ ; return 0;}
+#define exec(...) int main(int argc, char *argv[]) { exp(__VA_ARGS__) exp(return 0) }
 #define set(x, y) using x = y;
+#define lambda(fun, ...) [](__VA_ARGS__) { exp(fun) }
+#define def(name, fun, ...) exp(constexpr auto name = lambda(fun, __VA_ARGS__))
 
-set(Cond, True)
+def(add, return x + y, auto&& x, auto&& y)
+def(one, return 1)
+def(printnl, cout<<x<<'\n', auto&& x)
+def(other, return add(1, one()))
+
+set(Cond, False)
 exec(
   printnl(
     if Cond do() (
-      add, 2, 3,
+      lambda(return 1),
         else
-      add, 2, 2
+      add, one(), other()
     )
   )
 )
