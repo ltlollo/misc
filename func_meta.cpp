74ea3729739b363e352c278d4442ccaf25bd81e2
ltlollo <ltlollo.it@gmail.com>
2014-10-23 17:57:09 +0200

    fun: (+ 1 c)

--- func_meta.cpp
+++ func_meta.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 using namespace fun;
 using namespace fun::cond;
-using Cond = cond::False;
 
 constexpr auto add = [](auto&& x, auto&& y) {
     return x + y;
@@ -17,7 +16,9 @@ auto printnl = [](auto&& x) {
 #define do >
 #define else Else(),
 #define exec(...) int main(int argc, char *argv[]) { __VA_ARGS__ ; return 0;}
+#define set(x, y) using x = y;
 
+set(Cond, True)
 exec(
   printnl(
     if Cond do() (
