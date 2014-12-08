5c43be74884ce435f8b4fe6491563105094f46b0
ltlollo <ltlollo.it@gmail.com>
2014-12-08 17:57:38 +0100

    add a return

--- threads.cpp
+++ threads.cpp
@@ -16,7 +16,10 @@ template<size_t N, size_t... Ns> struct GenSeq : GenSeq<N-1, N-1, Ns...>{};
 template<size_t... Ns> struct GenSeq<0, Ns...>{ using type = Seq<Ns...>; };
 template<size_t N> using make_seq_t = typename GenSeq<N>::type;
 template<typename Ret, typename... Args> using fn_t = Ret(*)(Args...);
-template<typename T, size_t... N> const constexpr void caller(T* t) {
+template<typename T, size_t... N> const constexpr auto caller(T* t) {
+    t->r = t->call(Seq<N...>{});
+}
+template<typename T, size_t... N> const constexpr void vcaller(T* t) {
     t->call(Seq<N...>{});
 }
 
@@ -27,22 +30,47 @@ struct Function<Ret(T::*)(Args...) const> {
     using ptr_t = fn_t<Ret, Args...>;
 };
 
-template<typename R, typename F, typename... Args>
+template<typename Ret, typename F, typename... Args>
 struct FunStore {
     F f;
     std::tuple<Args...> args;
     pthread_t t;
+    Ret r;
 
-    R operator()() {
+    Ret operator()() {
         return call(make_seq_t<sizeof...(Args)>{});
     }
-    template<size_t... Ns> R call(Seq<Ns...>) {
+    template<size_t... Ns> Ret call(Seq<Ns...>) {
         return f(std::get<Ns>(args)...);
     }
-    template<size_t... Ns>
-    void call_async(Seq<Ns...>) {
+    template<size_t... Ns> void call_async(Seq<Ns...>) {
         pthread_create(&t, nullptr,
-            (fn_t<void*, void*>)caller<FunStore<R, F, Args...>, Ns...>,
+            (fn_t<void*, void*>)caller<FunStore<Ret, F, Args...>, Ns...>,
+            (void*)this);
+    }
+    void async() {
+        return call_async(make_seq_t<sizeof...(Args)>{});
+    }
+    void join() {
+        pthread_join(t, nullptr);
+    }
+};
+
+template<typename F, typename... Args>
+struct FunStore<void, F, Args...> {
+    F f;
+    std::tuple<Args...> args;
+    pthread_t t;
+
+    void operator()() {
+        call(make_seq_t<sizeof...(Args)>{});
+    }
+    template<size_t... Ns> void call(Seq<Ns...>) {
+        f(std::get<Ns>(args)...);
+    }
+    template<size_t... Ns> void call_async(Seq<Ns...>) {
+        pthread_create(&t, nullptr,
+            (fn_t<void*, void*>)vcaller<FunStore<void, F, Args...>, Ns...>,
             (void*)this);
     }
     void async() {
@@ -60,11 +88,11 @@ const constexpr auto make_function(F f, Args... args) {
     return FunStore<ret_t, fun_t, Args...>{(fun_t)f, make_tuple(args...)};
 }
 
-struct Some { string s; void print() {  for(;;) cout << s << endl; } };
+struct Some { string s; void print() {  while(true) cout << s << endl;} };
 
 int main(int argc, char *argv[]) {
     Some a1{"first"}, a2{"second"}, a3{"third"};
-    auto f = [](Some& i) { i.print(); };
+    auto f = [](Some& i) {i.print(); };
     auto t1 = make_function(f, a1);
     auto t2 = make_function(f, a2);
     auto t3 = make_function(f, a3);
@@ -74,7 +102,6 @@ int main(int argc, char *argv[]) {
     t3.async();
 
     t1.join();
-
     return 0;
 }
 
