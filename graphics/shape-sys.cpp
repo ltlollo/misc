72a26ea92c4a3f7ce50a54e1286ff678d46bfd6a
ltlollo <ltlollo.it@gmail.com>
2015-03-10 13:30:23 +0100

    parse the shape grammar

--- graphics/shape-sys.cpp
+++ graphics/shape-sys.cpp
@@ -3,6 +3,8 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <iterator>
+#include <iostream>
+#include <map>
 
 // gpp shape-sys.cpp $(pkg-config --libs sfml-all) && ./shape-sys
 
@@ -10,15 +12,7 @@ constexpr unsigned wh{600}, ww{800};
 
 using namespace std;
 
-enum PolyT { None = 0, Point, Line, Tri, Quad, Penta, Hexa };
-
-struct Shape : public vector<sf::Vertex> {
-    using vector<sf::Vertex>::vector;
-    PolyT type() const {
-        return (PolyT)this->size();
-    }
-};
-
+using Shape = vector<sf::Vertex>;
 using Shapes = vector<Shape>;
 
 sf::Vertex mid(const sf::Vertex& fst, const sf::Vertex& snd) {
@@ -28,79 +22,11 @@ sf::Vertex mid(const sf::Vertex& fst, const sf::Vertex& snd) {
     }};
 }
 
-template<PolyT T> struct Rule {
-    static Shapes next(const Shape& s) {
-        return {s};
-    }
-};
-
-template<> struct Rule<PolyT::Tri> {
-    static Shapes next(const Shape& shape) {
-        auto res = Shapes{};
-        auto intersec_fst = mid(shape[0], shape[1]);
-        auto intersec_snd = mid(shape[0], shape[2]);
-        auto intersec_trd = mid(shape[1], shape[2]);
-        res = Shapes{
-              {shape[0], intersec_fst, intersec_snd}
-             ,{shape[1], intersec_fst, intersec_trd}
-             ,{shape[2], intersec_snd, intersec_trd}
-        };
-        return res;
-    }
-};
-
-template<> struct Rule<PolyT::Quad> {
-    static Shapes next(const Shape& shape) {
-        auto res = Shapes{};
-        auto intersec_fst = mid(shape[1], shape[2]);
-        res = Shapes{
-             {shape[0], shape[1], intersec_fst}
-            ,{shape[0], intersec_fst, shape[3]}
-            ,{intersec_fst, shape[3], shape[2]}
-        };
-        return res;
-    }
-};
-
-Shapes match(const Shape& shape) {
-    switch (shape.type()) {
-        case PolyT::Tri:
-            return Rule<PolyT::Tri>::next(shape);
-        case PolyT::Quad:
-            return Rule<PolyT::Quad>::next(shape);
-        case PolyT::Penta:
-            return Rule<PolyT::Penta>::next(shape);
-        default:
-            return Rule<None>::next(shape);
-    }
-}
-
-Shapes iterate(const Shapes& state) {
-    auto shapes = Shapes{};
-    auto next = Shapes{};
-    for (const auto& it: state) {
-        next = move(match(it));
-        shapes.insert(end(shapes)
-                ,std::make_move_iterator(begin(next))
-                ,std::make_move_iterator(end(next))
-                );
-    }
-    return shapes;
-}
-
-Shapes iterate(const Shapes& state, unsigned depth) {
-    auto res = state;
-    for (unsigned i = 0; i < depth; ++i) {
-        res = move(iterate(res));
-    }
-    return res;
-}
-
 void drawShapes(sf::RenderWindow& win, const Shapes& shapes) {
     for(const auto& shape: shapes) {
         for (unsigned i = 0; i < shape.size() - 1; ++i) {
             win.draw(&shape[i], 2, sf::Lines);
-        } if (shape.type() > PolyT::Line) {
+        } if (shape.size() > 2) {
             auto last = Shape{shape[shape.size()-1], shape[0]};
             win.draw(&last[0], 2, sf::Lines);
         }
@@ -111,18 +37,156 @@ void drawShapes(sf::RenderWindow& win, const Shapes& shapes) {
  * h_|   | _f
  *   |_ _|
  *   D g C
- * How could a grammr rule be introduced: LHS "}A{e}B{f}C{g}D{h" -> RHS "Adf,Bde,Cef,.."
+ * How could a grammr rule be introduced: LHS "AeBfCgDh" -> RHS "Adf,Bde,Cef,.."
  */
 
+bool is_vertex(char it) {
+    return (it >= 'A' && it <= 'Z');
+}
+
+bool is_mid(char it) {
+    return (it >= 'a' && it <= 'z');
+}
+
+struct Parser {
+    bool do_nothing{false};
+    unsigned type{0};
+    string lhs;
+    vector<string> vrhs;
+    std::map<char, sf::Vertex> vmap;
+    Parser() : do_nothing{true} {
+        // the identity parser p().apply(shape) -> [shape]
+        // used my map<?, Parser>
+    }
+
+    Parser(const string& rule) {
+        string rhs;
+        auto it = std::find(begin(rule), end(rule), '>');
+        if (it > end(rule)-1) {
+            throw std::runtime_error("No sparator\nHint: lhs>[rhs]");
+        }
+        lhs = {begin(rule), it};
+        rhs = {it+1, end(rule)};
+        type = std::count_if(begin(lhs), end(lhs), is_vertex);
+        for (const auto& it: rhs) {
+            if (std::none_of(begin(lhs), end(lhs), [&](const auto& s){
+                return (s == it) || it == ',';
+            })) {
+                throw std::runtime_error("Unknown symbol: " + string{it});
+            }
+        }
+        auto adj_mids = std::adjacent_find(begin(lhs), end(lhs),
+                                           [](char f, char s){
+            return is_mid(f) && is_mid(s);
+        });
+        if (adj_mids != end(lhs)) {
+            throw std::runtime_error("Adjacent middle-points: " +
+            string(adj_mids, adj_mids+2) + "\nHint: ([A-Z][a-z]?)*");
+        }
+        if (type < 2 && lhs.size() != type) {
+            throw std::runtime_error("Points cannot be devided");
+        }
+        string curr;
+        for (auto it = begin(rhs); it != end(rhs); ++it) {
+            if (*it == ',') {
+                vrhs.push_back(move(curr));
+                curr = string{};
+            } else {
+                curr.push_back(*it);
+            }
+        }
+        vrhs.push_back(move(curr));
+        vrhs.erase(std::remove_if(begin(vrhs), end(vrhs), [](const auto& it){
+            return it.empty();
+        }), end(vrhs));
+
+        if (!lhs.empty()) { // this is a microopt and should be explained
+            lhs.push_back(lhs[0]);
+        }
+    }
+    Shapes apply(const Shape& shape) {
+        if (do_nothing) {
+            return {shape};
+        }
+        auto res = Shapes{};
+        if (vrhs.empty()) {
+            return res;
+        }
+        res.reserve(vrhs.size());
+        unsigned i = 0;
+        for(const auto& it: shape) {
+            while(!is_vertex(lhs[i])) {
+                ++i;
+            }
+            vmap[lhs[i]] = it;
+            ++i;
+        }
+        for(auto it = begin(lhs); it != end(lhs); ++it) {
+            if (is_mid(*it)) {
+                vmap[*it] = mid(vmap[*(it-1)], vmap[*(it+1)]);
+            }
+        }
+        for (const auto& it: vrhs) {
+            auto curr_shape = Shape{};
+            curr_shape.reserve(it.size());
+            for (const auto& sv: it) {
+                curr_shape.push_back(vmap[sv]);
+            }
+            res.push_back(move(curr_shape));
+        }
+        return res;
+    }
+};
+
+struct Grammar {
+    std::map<unsigned, Parser> pmap;
+    Grammar(const vector<Parser>& rules) {
+        for (auto it_f = begin(rules); it_f != end(rules); ++it_f) {
+            for (auto it_s = it_f+1; it_s != end(rules); ++it_s) {
+                if (it_f->type == it_s->type) {
+                    throw std::runtime_error("Non unique rule: " + it_f->lhs +
+                    " " + it_s->lhs);
+                }
+            }
+        }
+        for (const auto& it: rules) {
+            pmap[it.type] = it;
+        }
+    }
+    Shapes next(const Shape& shape) {
+        return pmap[shape.size()].apply(shape);
+    }
+    Shapes iterate(const Shapes& state) {
+        auto shapes = Shapes{};
+        auto curr = Shapes{};
+        for (const auto& it: state) {
+            curr = this->next(it);
+            shapes.insert(end(shapes)
+                    ,std::make_move_iterator(begin(curr))
+                    ,std::make_move_iterator(end(curr))
+                    );
+        }
+        return shapes;
+    }
+    Shapes iterate(const Shapes& state, unsigned depth) {
+        auto res = state;
+        for (unsigned i = 0; i < depth; ++i) {
+            res = move(this->iterate(res));
+        }
+        return res;
+    }
+};
 
 int main(int argc, char *argv[]) {
     sf::RenderWindow window{{ww, wh}, "shapes"};
     sf::Event event;
-
+    auto g = Grammar{{
+        Parser("AaBcCd>Aad,Bac,Ccd")
+    }};
     auto first = Shapes{{
         {{300.,100.}}, {{600.,400.}}, {{100.,400.}}
     }};
-    auto shapes = iterate(first, 7);
+    auto shapes = g.iterate(first, 10);
 
     while(window.isOpen()) {
         while (window.pollEvent(event)) {
