52696d7d59d8ea30f28b5c311888c8f57206e7c2
ltlollo <ltlollo.it@gmail.com>
2015-04-16 14:20:05 +0200

    provide better v alignenment

--- silly/count_100_to_1.cpp
+++ silly/count_100_to_1.cpp
@@ -1,16 +1,15 @@
-#include <iostream>
-using namespace std;
-
-int main(int, char                   *[]) {
+#include                         <iostream>
+using namespace std                       ;
+int main(int, char                    *[]){
     try                                   {
-                           #define i v= 100, i
-    for (int i = 0;;) {             v=v
-                ?[  &] (){ cout  <<  v
-                                 <<'\v'
-                        ;return --v
-           ;}( ):[  =]  (){ throw v
-                         ;return v
-   ;}();}}             catch(int v
-     ){                  ;return v
-                                ;}
-                                 }
+                  #define i v        ='d',_
+    for (int i = 0                     ;;){
+                            v             =
+                            v       ?[&](){
+                      cout<<v            <<
+                          '\v'            ;
+                    return--v   ;}():[=](){
+                      throw v             ;
+                     return v       ;}();}}
+                  catch(int v            ){
+                     return v           ;}}
