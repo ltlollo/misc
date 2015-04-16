95ce9cb6de1f9018d0f22ba45cf77e6f541d80a4
ltlollo <ltlollo.it@gmail.com>
2015-04-16 13:54:38 +0200

    solve the hard interview challange

---
+++ silly/count_100_to_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(int, char                   *[]) {
+    try                                   {
+                           #define i v= 100, i
+    for (int i = 0;;) {             v=v
+                ?[  &] (){ cout  <<  v
+                                 <<'\v'
+                        ;return --v
+           ;}( ):[  =]  (){ throw v
+                         ;return v
+   ;}();}}             catch(int v
+     ){                  ;return v
+                                ;}
+                                 }
