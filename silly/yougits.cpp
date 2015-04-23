882d947faa203004c85318c479c84d6de810d8be
ltlollo <ltlollo.it@gmail.com>
2015-04-23 22:06:34 +0200

    s: restructure

--- silly/yougits.cpp
+++ silly/yougits.cpp
@@ -7,29 +7,23 @@
 
 using namespace std;
 
-void initterm() {
-    initscr();
-    erase();
-    refresh();
-    cbreak();
-    noecho();
-    keypad(stdscr, TRUE);
-    scrollok(stdscr, TRUE);
-}
-
 struct CursesWin {
-    CursesWin() { initterm(); }
+    CursesWin() {
+        initscr();
+        erase();
+        refresh();
+        cbreak();
+        noecho();
+        keypad(stdscr, TRUE);
+        scrollok(stdscr, TRUE);
+    }
     ~CursesWin() { endwin(); }
 };
 
-int main(int argc, char *argv[]) {
-    if (argc - 1 < 1) {
-        printf("USAGE: %s file\nSCOPE: 攻殻機動隊 typing sim\n", argv[0]);
-        return 1;
-    }
+void print_forever(const string& fname) {
     auto l = locale("");
     locale::global(l);
-    wifstream file(argv[1]);
+    wifstream file(fname);
     file.imbue(l);
     noskipws(file);
     if (!file.is_open()) {
@@ -37,12 +31,20 @@ int main(int argc, char *argv[]) {
     }
     vector<wchar_t> buf(istreambuf_iterator<wchar_t>{file}, {});
     auto win = CursesWin();
+    cchar_t c;
+    for (size_t i = 0; getch(); i = (i+1)%buf.size()) {
+        c = { 0, buf[i] };
+        add_wch(&c);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc - 1 < 1) {
+        printf("USAGE: %s file\nSCOPE: 攻殻機動隊 typing sim\n", argv[0]);
+        return 1;
+    }
     try {
-        cchar_t c;
-        for (size_t i = 0; getch(); i = (i+1)%buf.size()) {
-            c = { 0, buf[i] };
-            add_wch(&c);
-        } 
+        print_forever(string(argv[1]));
     } catch (runtime_error& e) {
         printf(e.what());
     } catch (...) {
