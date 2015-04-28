e361b8f2b6c6c00f83e137b7cf1785c55a12f33f
ltlollo <ltlollo.it@gmail.com>
2015-04-28 13:26:14 +0200

    document what the code does

--- silly/count_100_to_1.cpp
+++ silly/count_100_to_1.cpp
@@ -1,3 +1,18 @@
+// #include                         <iostream>
+// using namespace std                       ;
+// int main(int, char                    *[]){
+//     try                                   {
+//                   #define i v        ='d',_
+//     for (int i = 0                     ;;){
+//                             v             =
+//                             v       ?[&](){
+//                       cout<<v            <<
+//                           '\v'            ;
+//                     return--v   ;}():[=](){
+//                       throw v             ;
+//                      return v       ;}();}}
+//                   catch(int v            ){
+//                      return v           ;}}
 #include                         <iostream>
 using namespace std                       ;
 int main(int, char                    *[]){
