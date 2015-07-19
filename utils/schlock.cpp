06a2e3eaaa80755fb63c9d6e2cc053d0b94a5135
ltlollo <ltlollo.it@gmail.com>
2015-07-19 04:32:47 +0200

    untested insomniac garbage

--- utils/schlock.cpp
+++ utils/schlock.cpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <string>
+#include <iostream>
 #include <png++/png.hpp>
 #include <stdlib.h>
 
@@ -12,6 +13,8 @@ using png_t = png::image<px_t>;
 inline bool bit_val(const px_t& px) {
     return (px.red ^ px.blue ^ px.green)&1;
 }
+
+struct bound { size_t hlb, hrb, vub, vlb; };
 
 inline void swap_bits(px_t& f, px_t& s) {
     bool tmp = f.red&1;
@@ -72,11 +75,11 @@ void encode(const vector<bool>& msg, size_t& pos,
     size_t hlb_orig = hlb, hrb_orig = hrb, vub_orig = vub, vlb_orig = vlb;
     auto vcount = lgtr(img, hlb, hrb, vub, vlb);
     if (vcount != Equal && vcount != msg[pos++]) {
-            for (size_t i = vub; i < vlb+1; ++i) {
-                while(hlb <= hrb) {
-                    swap_bits(img[i][hlb++], img[i][hrb--]);
-                }
+        for (size_t i = vub; i < vlb+1; ++i) {
+            while(hlb <= hrb) {
+                swap_bits(img[i][hlb++], img[i][hrb--]);
             }
+        }
     } else {
         hlb = (hlb+hrb)/2+1;
         hrb = hrb - (hrb-hlb_orig)/2-1;
@@ -106,11 +109,105 @@ void encode(const vector<bool>& msg, size_t& pos,
     encode(msg, pos, img, hrb+1, hrb_orig, vlb+1, vlb_orig);
 }
 
+Count lgtr(const png_t& img, bound b) {
+    size_t right_count = 0, letf_count = 0;
+    for (size_t i = b.vub; i < b.vlb+1; ++i) {
+        while(b.hlb <= b.hrb) {
+            letf_count += bit_val(img[i][b.hlb++]);
+            right_count += bit_val(img[i][b.hrb--]);
+        }
+    }
+    if (letf_count > right_count) {
+        return Left;
+    } else if (letf_count < right_count) {
+        return Right;
+    } else {
+        return Equal;
+    }
+}
+
+Count ugtd(const png_t& img, bound b) {
+    size_t upper_count = 0, lower_count = 0;
+    while(b.vub <= b.vlb) {
+        for (size_t i = b.hlb; i < b.hrb+1; ++i) {
+            upper_count += bit_val(img[b.vub][i]);
+            lower_count += bit_val(img[b.vlb][i]);
+        }
+        b.vub++;
+        b.vlb--;
+    }
+    if (upper_count > lower_count) {
+        return Up;
+    } else if (lower_count < upper_count) {
+        return Low;
+    } else {
+        return Equal;
+    }
+}
+
+void encoder(const vector<bool>& msg, size_t& mpos,
+            png_t& img, const size_t bpos, vector<bound>& bounds) {
+    if (mpos >= msg.size()) {
+        return;
+    }
+    size_t hlb = bounds[bpos].hlb, hrb = bounds[bpos].hrb,
+           vub = bounds[bpos].vub, vlb = bounds[bpos].vlb;
+    auto vcount = lgtr(img, bounds[bpos]);
+    if (vcount != Equal && vcount != msg[mpos++]) {
+            for (size_t i = vub; i < vlb+1; ++i) {
+                while(hlb <= hrb) {
+                    swap_bits(img[i][hlb++], img[i][hrb--]);
+                }
+            }
+    } else {
+        hlb = (hlb+hrb)/2+1;
+        hrb = hrb - (hrb-bounds[bpos].hlb)/2-1;
+    }
+    if (mpos >= msg.size()) {
+        return;
+    }
+    vcount = ugtd(img, bounds[bpos]);
+    if (vcount != Equal && vcount != msg[mpos++]) {
+        while(vub <= vlb) {
+            for (size_t i = bounds[bpos].hlb; i < bounds[bpos].hrb+1; ++i) {
+                swap_bits(img[vub][i], img[vlb][i]);
+            }
+            vub++;
+            vlb--;
+        }
+    } else {
+        vub = (vub+vlb)/2+1;
+        vlb = vlb - (vlb-bounds[bpos].vub)/2-1;
+    }
+    if (bounds[bpos].hlb+2 >= bounds[bpos].hrb ||
+        bounds[bpos].vub+2 >= bounds[bpos].vlb) {
+        return;
+    }
+    bounds.emplace_back(bound{bounds[bpos].hlb,hlb-1,bounds[bpos].vub,vub-1});
+    bounds.emplace_back(bound{hrb+1,bounds[bpos].hrb,bounds[bpos].vub,vub-1});
+    bounds.emplace_back(bound{bounds[bpos].hlb,hlb-1,vlb+1,bounds[bpos].vlb});
+    bounds.emplace_back(bound{hrb+1,bounds[bpos].hrb,vlb+1,bounds[bpos].vlb});
+}
+
+
+size_t encode(png_t& img, const vector<bool>& msg) {
+    if (msg.empty()) {
+        return 0;
+    }
+    size_t mpos = 0, bpos = 0;
+    vector<bound> bounds;
+    bounds.reserve(msg.size());
+    bounds.emplace_back(bound{0, img.get_width()-1, 0, img.get_height()-1});
+    do {
+        encoder(msg, mpos, img, bpos++, bounds);
+    } while (mpos != msg.size() || bpos != bounds.size());
+    return mpos;
+}
 
 int main(int , char *[]) {
     auto msg = vector<bool>(20, true);
     auto img = png_t(string(getenv("HOME")) + "/Images/45.png");
-    size_t size = 0;
-    encode(msg, size, img, 0, img.get_width()-1, 0, img.get_height()-1);
+    auto size = encode(img, msg);
+    cerr << "[I]: bits encoded " << size << '\n';
     return 0;
 }
