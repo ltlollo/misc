bf0073d104b79f626bd68313b26641f7a867d63c
ltlollo <ltlollo.it@gmail.com>
2015-07-31 04:50:39 +0200

    b: incorrect rounding, add one grayscale level

--- silly/quinalli.cpp
+++ silly/quinalli.cpp
@@ -8,9 +8,9 @@
 using namespace std;
 using px_t = png::rgb_pixel;
 using png_t = png::image<px_t>;
-constexpr char lo = 32, mid = 46,  hi = 120;
+constexpr char lo = 32, lm = 46, hm = 58, hi = 120;
 double luma(const px_t& p) {return 0.2126*p.red+0.7152*p.green+0.0722*p.blue; }
-char to_ch(double i) { return (i >= 98) ? hi : (i > 68 && i < 98) ? mid : lo; }
+char to_ch(double i) {return(i>=123)?hi:(i>=98&&i<123)?hm:(i>=68&&i<98)?lm:lo;}
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
@@ -19,16 +19,15 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     png_t img(argv[1]);
-    double s = stoll(argv[2]);
-    size_t w = img.get_width(), h = img.get_height();
-    if (!w || !h) {
+    double s = stol(argv[2]), w = img.get_width(), h = img.get_height();
+    if (!w || !h || !s) {
         cerr << argv[1] << "too small\n";
         return 1;
     }
-    printf("P5 %ld %ld %d", size_t(s+1), size_t(s/w*h), unsigned(hi));
-    for (size_t i = 0; i < s/w*h; ++i) {
+    printf("P5 %ld %ld %d", size_t(s+1), size_t(s*(h/w)), unsigned(hi));
+    for (double i = 0; i < s*(h/w); ++i) {
         cout << '\n';
-        for (size_t j = 0; j < s; ++j) {
+    for (double j = 0; j < s; ++j) {
             cout << to_ch(luma(img[i/s*w][j/s*w]));
         }
     }
