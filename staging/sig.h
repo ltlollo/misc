fdf053be1df1fc6bb5c2659567e9ff52bfc78da1
ltlollo <ltlollo.it@gmail.com>
2015-09-15 16:21:48 +0200

    add a code sample

--- staging/sig.h
+++ staging/sig.h
@@ -5,6 +5,33 @@
 #include <type_traits>
 #include "extra/fntraits.h"
 
+/* Scope:
+ *
+ * Tiny pipe/signal lib
+ *
+ * Example(code):
+ *
+ * void f() { std::cout << "Hello "; }
+ * void g() { std::cout << " world\n"; }
+ * int h(int i) { return i+1; }
+ * void i(int j) { std::cout << "j is " << j << '\n'; }
+ * int main(int , char *[]) {
+ *     auto sigf = connect(f, g);                       sigf();
+ *     auto d = sigf.disconnect();                      sigf();
+ *     sigf.reconnect([](){ std::cout << " what? "; }); sigf();
+ *     sigf.reconnect(d);                               sigf();
+ *     auto hh = connect(h, h); {
+ *     auto hhi = connect(hh, i);
+ *     auto d = hhi.disconnect();  hhi(3);
+ *     hhi.reconnect(d);           hhi(3); } {
+ *     auto hhi = pipe(h) | h | i; hhi(3);
+ *     }
+ *     auto hhi = pipe(hh) | i;    hhi(3); pipe(2) | hhi ; pipe(2) | i;
+ *     return 0;
+ * }
+ */
+
+
 template<typename T> struct Compose {
     template<typename G, typename F, typename... Args>
     static constexpr auto call(G g, F f, Args&&... args) {
