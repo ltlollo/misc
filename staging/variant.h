56fbdaa1be0051fe9b5b2d8f447e26bfee484e3c
L <ltlollo.it@gmail.com>
2015-10-20 09:24:52 +0200

    clean

--- staging/variant.h
+++ staging/variant.h
@@ -3,35 +3,42 @@
 
 #include <vector>
 #include <tuple>
-#include <cstddef>
 
-template<std::size_t N, typename T, typename... Ts> struct Adder;
+template<std::size_t N, typename T, typename... Ts> struct Find_helper;
 template<std::size_t N, typename T, typename... Ts>
-struct Adder<N, T, T, Ts...> {
-    template<typename V, typename U> static auto add(V& tp, U&& val) {
-        std::get<N>(tp).emplace_back(std::forward<U>(val));
-    }
+struct Find_helper<N, T, T, Ts...> {
+    static constexpr size_t value = N;
 };
 template<std::size_t N, typename T, typename TT, typename... Ts>
-struct Adder<N, T, TT, Ts...> {
-    template<typename V, typename U> static auto add(V& tp, U&& val) {
-        Adder<N+1, T, Ts...>::add(tp, std::forward<U>(val));
-    }
+struct Find_helper<N, T, TT, Ts...> {
+    static constexpr size_t value = Find_helper<N+1, T, Ts...>::value;
+};
+template<typename T, typename... Ts> struct Find {
+    static constexpr size_t value = Find_helper<0, T, Ts...>::value;
 };
 
-template<std::size_t N, std::size_t Size> struct Apply {
+template<std::size_t N> struct Apply_helper;
+template<std::size_t N> struct Apply_helper {
     template<typename F, typename T> static auto call(T& tp, F f) {
-        auto& vref = std::get<N>(tp);
+        auto& vref = std::get<std::tuple_size<T>::value-N>(tp);
         for (auto& ele: vref) {
             f(ele);
         }
-        Apply<N+1, Size>::call(tp, f);
+        Apply_helper<N-1>::call(tp, f);
     }
 };
-template<std::size_t N> struct Apply<N, N> {
-    template<typename F, typename T> static auto call(T&, F) {
+template<> struct Apply_helper<1> {
+    template<typename F, typename T> static auto call(T& tp, F f) {
+        auto& vref = std::get<std::tuple_size<T>::value-1>(tp);
+        for (auto& ele: vref) {
+            f(ele);
+        }
     }
 };
+template<typename T, typename F>
+static auto constexpr apply(T& tp, F f) {
+    return Apply_helper<std::tuple_size<T>::value>::call(tp, f);
+}
 
 template<typename... Ts> struct Unique;
 template<typename T, typename... Ts> struct Unique<T, T, Ts...> {
@@ -45,17 +52,18 @@ template<typename T> struct Unique<T>{
     static constexpr bool value = true;
 };
 
-template<typename... Ts>
-struct vec_variant {
+template<typename... Ts> struct vec_variant {
     static_assert(Unique<Ts...>::value, "non unique types");
     std::tuple<std::vector<Ts>...> data;
     template<typename F> auto foreach(F f) {
-        Apply<0, sizeof...(Ts)>::call(data, f);
+        apply(data, f);
     }
     template<typename T> auto push(T&& val) {
-        Adder<0, T, Ts...>::add(data, std::forward<T>(val));
+        std::get<Find<T, Ts...>::value>(data).emplace_back(val);
     }
-
+    template<typename T> auto& get() {
+        return std::get<Find<T, Ts...>::value>(data);
+    }
 };
 
 #endif // VARIANT_H
