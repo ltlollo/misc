2522c73148206399c99c54cc692468d048986808
L <ltlollo.it@gmail.com>
2015-10-23 17:25:32 +0200

    add view

--- staging/variant.h
+++ staging/variant.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <tuple>
+#include <functional>
 
 template<typename T, typename... Ts> struct Find;
 template<std::size_t N, typename T, typename... Ts> struct Find_helper;
@@ -10,23 +11,46 @@ template<std::size_t N> struct Apply_helper;
 template<typename T, typename F> static auto constexpr apply(T& tp, F f);
 template<typename... Ts> struct Unique;
 template<typename... Ts> struct Vec_variant;
+template<typename... Ts> struct Vec_variant_view;
 
 template<typename... Ts> struct Vec_variant {
     static_assert(Unique<Ts...>::value, "non unique types");
     std::tuple<std::vector<Ts>...> data;
     template<typename F> auto foreach(F f) {
-        return apply(data, f);
+        return apply(data, [=](auto &vec){
+            for (auto &ele: vec) {
+                f(ele);
+            }
+        });
     }
     template<typename T> auto push(T&& val) {
         return std::get<Find<T, Ts...>::value>(data).emplace_back(val);
     }
-    template<typename T> auto& get() {
+    template<typename T> constexpr auto& get() {
         return std::get<Find<T, Ts...>::value>(data);
+    }
+    template<typename T> constexpr auto* get_ptr() {
+        return &std::get<Find<T, Ts...>::value>(data);
+    }
+    template<typename... Us> auto get_ptrs() {
+        return Vec_variant_view<Us...> { std::make_tuple(get_ptr<Us>()...) };
+    }
+};
+
+template<typename... Ts> struct Vec_variant_view {
+    std::tuple<std::vector<Ts>*...> data;
+    template<typename F> auto foreach(F f) {
+        return apply(data, [=](auto &vec){
+            for (auto &ele: *vec) {
+                f(ele);
+            }
+        });
     }
 };
 
 template<typename T, typename... Ts> struct Find {
     static constexpr size_t value = Find_helper<0, T, Ts...>::value;
+    static_assert(value != sizeof...(Ts), "type not in variant");
 };
 
 template<std::size_t N, typename T, typename... Ts>
@@ -37,26 +61,25 @@ template<std::size_t N, typename T, typename TT, typename... Ts>
 struct Find_helper<N, T, TT, Ts...> {
     static constexpr size_t value = Find_helper<N+1, T, Ts...>::value;
 };
+template<std::size_t N, typename T> struct Find_helper<N, T> {
+    static constexpr size_t value = N;
+};
 
 template<typename T, typename F> static auto constexpr apply(T& tp, F f) {
     return Apply_helper<std::tuple_size<T>::value>::call(tp, f);
 }
 
 template<std::size_t N> struct Apply_helper {
-    template<typename F, typename T> static auto call(T& tp, F f) {
+    template<typename F, typename T> static constexpr auto call(T& tp, F f) {
         auto& vref = std::get<std::tuple_size<T>::value-N>(tp);
-        for (auto& ele: vref) {
-            f(ele);
-        }
+        f(vref);
         Apply_helper<N-1>::call(tp, f);
     }
 };
 template<> struct Apply_helper<1> {
-    template<typename F, typename T> static auto call(T& tp, F f) {
+    template<typename F, typename T> static constexpr auto call(T& tp, F f) {
         auto& vref = std::get<std::tuple_size<T>::value-1>(tp);
-        for (auto& ele: vref) {
-            f(ele);
-        }
+        f(vref);
     }
 };
 
