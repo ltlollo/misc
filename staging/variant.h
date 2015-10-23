611c8cb86a95480231981b32d031c58e37638ef9
L <ltlollo.it@gmail.com>
2015-10-23 14:59:25 +0200

    reorder for better readab. capital datsstruct

--- staging/variant.h
+++ staging/variant.h
@@ -4,7 +4,31 @@
 #include <vector>
 #include <tuple>
 
+template<typename T, typename... Ts> struct Find;
 template<std::size_t N, typename T, typename... Ts> struct Find_helper;
+template<std::size_t N> struct Apply_helper;
+template<typename T, typename F> static auto constexpr apply(T& tp, F f);
+template<typename... Ts> struct Unique;
+template<typename... Ts> struct Vec_variant;
+
+template<typename... Ts> struct Vec_variant {
+    static_assert(Unique<Ts...>::value, "non unique types");
+    std::tuple<std::vector<Ts>...> data;
+    template<typename F> auto foreach(F f) {
+        return apply(data, f);
+    }
+    template<typename T> auto push(T&& val) {
+        return std::get<Find<T, Ts...>::value>(data).emplace_back(val);
+    }
+    template<typename T> auto& get() {
+        return std::get<Find<T, Ts...>::value>(data);
+    }
+};
+
+template<typename T, typename... Ts> struct Find {
+    static constexpr size_t value = Find_helper<0, T, Ts...>::value;
+};
+
 template<std::size_t N, typename T, typename... Ts>
 struct Find_helper<N, T, T, Ts...> {
     static constexpr size_t value = N;
@@ -13,11 +37,11 @@ template<std::size_t N, typename T, typename TT, typename... Ts>
 struct Find_helper<N, T, TT, Ts...> {
     static constexpr size_t value = Find_helper<N+1, T, Ts...>::value;
 };
-template<typename T, typename... Ts> struct Find {
-    static constexpr size_t value = Find_helper<0, T, Ts...>::value;
-};
 
-template<std::size_t N> struct Apply_helper;
+template<typename T, typename F> static auto constexpr apply(T& tp, F f) {
+    return Apply_helper<std::tuple_size<T>::value>::call(tp, f);
+}
+
 template<std::size_t N> struct Apply_helper {
     template<typename F, typename T> static auto call(T& tp, F f) {
         auto& vref = std::get<std::tuple_size<T>::value-N>(tp);
@@ -35,11 +59,7 @@ template<> struct Apply_helper<1> {
         }
     }
 };
-template<typename T, typename F> static auto constexpr apply(T& tp, F f) {
-    return Apply_helper<std::tuple_size<T>::value>::call(tp, f);
-}
 
-template<typename... Ts> struct Unique;
 template<typename T, typename... Ts> struct Unique<T, T, Ts...> {
     static constexpr bool value = false;
 };
@@ -51,18 +71,5 @@ template<typename T> struct Unique<T> {
     static constexpr bool value = true;
 };
 
-template<typename... Ts> struct vec_variant {
-    static_assert(Unique<Ts...>::value, "non unique types");
-    std::tuple<std::vector<Ts>...> data;
-    template<typename F> auto foreach(F f) {
-        return apply(data, f);
-    }
-    template<typename T> auto push(T&& val) {
-        return std::get<Find<T, Ts...>::value>(data).emplace_back(val);
-    }
-    template<typename T> auto& get() {
-        return std::get<Find<T, Ts...>::value>(data);
-    }
-};
 
 #endif // VARIANT_H
