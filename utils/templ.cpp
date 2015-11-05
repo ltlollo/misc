d9582e2d0bb1ace86159daa854932bc4e1b5fd03
L <ltlollo.it@gmail.com>
2015-11-05 16:28:58 +0100

    s: cleanup

--- utils/templ.cpp
+++ utils/templ.cpp
@@ -5,29 +5,43 @@
 
 /*
  * Tiny templating program
+ * <html><body>
+ * # import=cat bash
+ * #    $import page.html
+ * #    for i in *; do
+ * #      echo "  <a href=\"$PWD/$i\">$i</a>"
+ * #    done
+ * </body></html>
+ * #python
+ * #from datetime import date
+ * #print(date.today())
  */
 
 int main(int argc, char *argv[]) {
     FILE *exec;
     std::string cmd, in, env;
     char quot = (argc-1) ? argv[1][0] : '#';
+    auto call_once = [&]() {
+        if (env.empty()) {
+            return;
+        }
+        if ((exec = popen(env.c_str(), "w")) == NULL) {
+            err(1, "%s", env.c_str());
+        }
+        if (!cmd.empty() &&
+            fwrite(cmd.c_str(), 1, cmd.size(), exec) != cmd.size()) {
+            (void)pclose(exec);
+            err(1, "%s", env.c_str());
+        }
+        if (pclose(exec) != 0) {
+            errx(1, "%s failed", cmd.c_str());
+        }
+        cmd.resize(0);
+        env.resize(0);
+    };
     while (std::getline(std::cin, in)) {
         if (in.empty() || in[0] != quot) {
-            if (!env.empty()) {
-                if ((exec = popen(env.c_str(), "w")) == NULL) {
-                    err(1, "%s", env.c_str());
-                }
-                if (!cmd.empty() && fwrite(cmd.c_str(), 1, cmd.size(), exec)
-                    != cmd.size()) {
-                    (void)pclose(exec);
-                    err(1, "%s", env.c_str());
-                }
-                if (pclose(exec) != 0) {
-                    errx(1, "%s failed", cmd.c_str());
-                }
-                cmd.resize(0);
-                env.resize(0);
-            }
+            call_once();
             printf("%s\n", in.c_str());
         } else {
             if (env.empty()) {
@@ -36,7 +50,7 @@ int main(int argc, char *argv[]) {
                 cmd += std::string(in.data()+1) + '\n';
             }
         }
-
     }
+    call_once();
     return 0;
 }
