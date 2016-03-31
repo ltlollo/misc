1d3e8e71b7c505a4551fe58eccca3efc6df45cbc
L <ltlollo.it@gmail.com>
2016-03-31 12:14:51 +0200

    pad

--- utils/wrapline.cpp
+++ utils/wrapline.cpp
@@ -1,20 +1,24 @@
 // gpp self
 
-#include <vector>
-#include <string>
-#include <iostream>
 #include <algorithm>
 #include <codecvt>
-#include <locale>
 #include <experimental/string_view>
+#include <iostream>
+#include <locale>
 #include <string.h>
+#include <string>
+#include <vector>
+
+using namespace std::experimental;
 
 int main(int argc, char *argv[]) {
     auto print_usage = [&]() {
-        std::wcerr << "Usage:\t" << argv[0] << L" N S\nScope:\tpresent"
-            " text using N runes per line, using S to indicate line"
-            " wrap.\n\tN<uint+>: runes per line (default: 79)\n\t"
-            "S<str>: newline indicator (default ' ↵')" << std::endl;
+        std::wcerr << "Usage:\t" << argv[0]
+                   << L" N S\nScope:\tpresent"
+                      " text using N runes per line, using S to indicate line"
+                      " wrap.\n\tN<uint+>: runes per line (default: 79)\n\t"
+                      "S<str>: newline indicator (default ' ↵')"
+                   << std::endl;
     };
     std::locale::global(std::locale(""));
     std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
@@ -23,8 +27,7 @@ int main(int argc, char *argv[]) {
         size_t res;
         char* end;
         if ((res = std::strtoll(argv[1], &end, 10)) != 0 &&
-            end == argv[1] + strlen(argv[1])) {
-            // out of range is not an error
+            end == argv[1] + strlen(argv[1])) { // OOR is not an error
             return res;
         }
         std::wcerr << "[E]: " << converter.from_bytes(argv[1]) << "; 0/NaN\n";
@@ -32,27 +35,41 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }() : 79;
     std::wstring sepa = (argc > 2) ? converter.from_bytes(argv[2]) : L" ↵";
-    if (max <= sepa.length() ) {
+    sepa += '\n';
+    if (max <= sepa.length()) {
         std::copy(std::istreambuf_iterator<wchar_t>{std::wcin}, {}, wc);
         std::wcerr << "[E]: separator size must be greater than line size\n";
         exit(EXIT_FAILURE);
     }
     for (std::wstring s; std::getline(std::wcin, s);) {
-        auto it = s.begin();
-        while (size_t(distance(it, s.end())) > max) {
-            auto rpos = std::experimental::wstring_view(&*it, max)
-                .find_last_of(' ');
+        auto it = std::begin(s);
+        auto se = std::find_if_not(it, std::end(s), std::iswspace);
+        auto d = size_t(se - it);
+        auto m = (se != std::end(s) && max > d) ? max - d : max;
+        auto fst = true;
+        while (size_t(distance(it, std::end(s))) > max) {
+            auto rpos = wstring_view(&*it, m).find_last_of(' ');
             if (rpos == std::wstring::npos) {
-                std::copy(it, it+max-sepa.length(), wc);
-                std::wcout << sepa << '\n';
-                it += max-sepa.length();
+                if (m != max && !fst) {
+                    std::copy(std::begin(s), se, wc);
+                }
+                std::copy(it, it + m - sepa.length() - 1, wc);
+                std::copy(std::begin(sepa), std::end(sepa), wc);
+                it += m - sepa.length() - 1;
             } else {
-                *(it+rpos) = '\n';
-                std::copy(it, it+rpos+1, wc);
-                it += rpos+1;
+                *(it + rpos) = '\n';
+                if (m != max && !fst) {
+                    std::copy(std::begin(s), se, wc);
+                }
+                std::copy(it, it + rpos + 1, wc);
+                it += rpos + 1;
             }
+            fst = false;
         }
-        std::copy(it, s.end(), wc);
+        if (m != max && !fst) {
+            std::copy(std::begin(s), se, wc);
+        }
+        std::copy(it, std::end(s), wc);
         std::wcout << std::endl;
     }
     return 0;
